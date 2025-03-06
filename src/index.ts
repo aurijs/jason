b1808220eaccbe1376b1b3df2650b4e03b6fc006
realfakenerd <lucas210898@hotmail.com>
2025-03-06 14:03:26 -0300

    chore: add gitignore files for node and python packages
    Add gitignore files to node-jason and python-jason packages.
    This prevents unnecessary files like logs, caches, and
    dependencies from being tracked in git, keeping the
    repository clean and focused on source code.

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as JasonDB } from "./core/main.js";
--- src/types/utils.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Represents a validation function type.
- *
- * @template T The type of data being validated.
- */
-export type ValidationFunction<T> = (item: T) => boolean;
