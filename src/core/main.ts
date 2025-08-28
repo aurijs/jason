58d1fdd5a21c1e426d30461d66f3639d367ce9ce
realfakenerd <lucas210898@hotmail.com>
2025-08-28 17:29:20 -0300

    ♻️ (core/main.ts): Refactor JasonDB initialization to improve code structure and readability

--- src/core/main.ts
+++ src/core/main.ts
@@ -1,6 +1,6 @@
 import { FileSystem } from "@effect/platform";
 import { NodeFileSystem } from "@effect/platform-node";
-import { Effect, Layer, Ref, Runtime } from "effect";
+import { Cause, Effect, Layer, Ref, Runtime } from "effect";
 import { readdir } from "node:fs/promises";
 import path from "node:path";
 import Collection from "../data/collection.js";
@@ -11,8 +11,8 @@ import type {
 } from "../types/index.js";
 import { DatabaseError } from "./errors.js";
 
-function ensureDataDirExists(base_path: string) {
-  return Effect.gen(function* () {
+const ensureDataDirExists = (base_path: string) =>
+  Effect.gen(function* () {
     const fs = yield* FileSystem.FileSystem;
 
     const exists = yield* fs.exists(base_path).pipe(
@@ -26,7 +26,52 @@ function ensureDataDirExists(base_path: string) {
       yield* fs.makeDirectory(base_path);
     }
   });
-}
+
+const make = <T>(
+  base_path: string,
+  collections_ref: Ref.Ref<Map<keyof T, Collection<T, keyof T>>>
+) => {
+  const collectionEffect = <K extends keyof T>(
+    name: K,
+    options: CollectionOptions<Document<T, K>> = {}
+  ) =>
+    Ref.get(collections_ref).pipe(
+      Effect.flatMap((existing_collections) => {
+        if (existing_collections.has(name)) {
+          return Effect.succeed(
+            existing_collections.get(name) as Collection<T, K>
+          );
+        }
+        const new_collection = new Collection<T, K>(base_path, name, options);
+        return Ref.updateAndGet(collections_ref, (map) =>
+          map.set(name, new_collection)
+        ).pipe(Effect.map(() => new_collection));
+      })
+    );
+
+  const listCollectionsEffect = Effect.gen(function* () {
+    const existing_collections = yield* collections_ref.get;
+    if (existing_collections.size > 0) {
+      return Array.from(existing_collections.keys());
+    }
+
+    const entries = yield* Effect.tryPromise({
+      try: async () => {
+        const entries = await readdir(base_path, { withFileTypes: true });
+        return entries
+          .filter((entry) => entry.isDirectory() && !entry.name.startsWith("_"))
+          .map((entry) => entry.name) as (keyof T)[];
+      },
+      catch: () => {
+        return [];
+      },
+    });
+
+    return entries;
+  });
+
+  return { collectionEffect, listCollectionsEffect };
+};
 
 /**
  * Creates a new JasonDB instance with the given configuration.
@@ -57,83 +102,44 @@ export async function Jason<T>(options: string | JasonDBOptions) {
 
     yield* ensureDataDirExists(base_path);
 
-    const listCollections = async () => {
-      const effect = Effect.gen(function* () {
-        const existing_collections = yield* collections_ref.get;
-        if (existing_collections.size > 0) {
-          return Array.from(existing_collections.keys());
-        }
-
-        const entries = yield* Effect.tryPromise({
-          try: async () => {
-            const entries = await readdir(base_path, { withFileTypes: true });
-            return entries
-              .filter(
-                (entry) => entry.isDirectory() && !entry.name.startsWith("_")
-              )
-              .map((entry) => entry.name) as (keyof T)[];
-          },
-          catch: () => {
-            return [];
-          },
-        });
-
-        return entries;
-      });
-
-      return Runtime.runPromise(runtime)(effect);
-    };
-
-    /**
-     * Retrieves or creates a collection in the database.
-     *
-     * If a collection with the given name does not exist, it initializes a new collection
-     * with the provided options and stores it. If the collection already exists, it returns the existing one.
-     *
-     * @param name - The name of the collection.
-     * @param options - Optional settings for the collection.
-     * @param options.initialData - An array of initial data to populate the collection.
-     * @param options.schema - A validation function for the collection's documents.
-     * @param options.concurrencyStrategy - The concurrency strategy to use for the collection.
-     * @param options.cacheTimeout - The cache timeout in milliseconds.
-     * @param options.generateMetadata - Whether to generate metadata for the collection.
-     * @param options.indices - An optional string of index definitions for the collection (e.g., ['++id', '&email', '*tags']).
-     * @returns The collection instance associated with the given name.
-     */
-    const collection = <K extends keyof T>(
-      name: K,
-      options: CollectionOptions<Document<T, K>> = {}
-    ) => {
-      const effect = Ref.get(collections_ref).pipe(
-        Effect.flatMap((existing_collections) => {
-          if (existing_collections.has(name)) {
-            return Effect.succeed(
-              existing_collections.get(name) as Collection<T, K>
-            );
-          }
-          const newCollection = new Collection<T, K>(base_path, name, options);
-          return Ref.updateAndGet(collections_ref, (map) =>
-            map.set(name, newCollection)
-          ).pipe(Effect.map(() => newCollection));
-        })
-      );
-
-      return Runtime.runSync(runtime)(effect);
-    };
+    const { collectionEffect, listCollectionsEffect } = make<T>(
+      base_path,
+      collections_ref
+    );
 
     return {
-      collection,
-      listCollections,
+      /**
+       * Retrieves or creates a collection in the database.
+       *
+       * If a collection with the given name does not exist, it initializes a new collection
+       * with the provided options and stores it. If the collection already exists, it returns the existing one.
+       *
+       * @param name - The name of the collection.
+       * @param options - Optional settings for the collection.
+       * @param options.initialData - An array of initial data to populate the collection.
+       * @param options.schema - A validation function for the collection's documents.
+       * @param options.concurrencyStrategy - The concurrency strategy to use for the collection.
+       * @param options.cacheTimeout - The cache timeout in milliseconds.
+       * @param options.generateMetadata - Whether to generate metadata for the collection.
+       * @param options.indices - An optional string of index definitions for the collection (e.g., ['++id', '&email', '*tags']).
+       * @returns The collection instance associated with the given name.
+       */
+      collection: <K extends keyof T>(
+        name: K,
+        options?: CollectionOptions<Document<T, K>>
+      ) => Runtime.runPromise(runtime)(collectionEffect(name, options)),
+      listCollections: () => Runtime.runPromise(runtime)(listCollectionsEffect),
     };
   });
 
   const AppLayer = Layer.mergeAll(NodeFileSystem.layer);
 
   return Effect.runPromise(program.pipe(Effect.provide(AppLayer))).catch(
-    (error) =>
-      new DatabaseError({
+    (error) => {
+      throw new DatabaseError({
         message: "Error initializing Database",
         cause: error,
-      })
+      });
+    }
   );
 }
