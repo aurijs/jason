e66399bc8ddb4f8e82d64c5287c100e128ae93fb
realfakenerd <lucas210898@hotmail.com>
2025-09-15 16:40:37 -0300

    feat(index): add index service for document indexing with B-trees
    Implement a new index service that creates B-tree instances for each indexed field,
    providing update and find operations to maintain and query document indices efficiently.

---
+++ src/layers/index.ts
@@ -0,0 +1,92 @@
+import { Effect, Schema } from "effect";
+import type { IndexDefinition } from "../types/metadata.js";
+import { makeBtreeService } from "./btree.js";
+
+export const makeIndexService = <Doc extends { id: string }>(
+  index_path: string,
+  index_definitions: Record<string, IndexDefinition>,
+  doc_schema: Schema.Schema<any, Doc>
+) =>
+  Effect.gen(function* () {
+    // For each field that needs being indexed, create a
+    // B-tree service instance
+    const btree_services = yield* Effect.all(
+      Object.fromEntries(
+        Object.entries(index_definitions).map(([field_name]) => {
+          const key_schema = (doc_schema as Schema.Struct<any>).fields[
+            field_name
+          ];
+          if (!key_schema) {
+            throw new Error(
+              `Field ${field_name} does not exist in document schema`
+            );
+          }
+
+          const btree_service_effect = makeBtreeService(
+            `${index_path}/${field_name}`,
+            key_schema,
+            8 // B-tree order
+          );
+          return [field_name, btree_service_effect];
+        })
+      )
+    );
+
+    /**
+     * Updates the index for a document.
+     * @param old_doc The old document.
+     * @param new_doc The new document.
+     * @returns An effect that updates the index.
+     */
+    const update = (old_doc: Doc | undefined, new_doc: Doc | undefined) =>
+      Effect.forEach(
+        Object.keys(index_definitions),
+        (field_name) =>
+          Effect.gen(function* () {
+            const btree = btree_services[field_name];
+            const old_value = old_doc?.[field_name as keyof Doc];
+            const new_value = new_doc?.[field_name as keyof Doc];
+
+            // If the value didn't change, do nothing
+            if (old_value === new_value) {
+              return;
+            }
+
+            if (old_value !== undefined) {
+              // TODO: Remove old value from index
+            }
+
+            // Insert new value into index
+            // The key is the value of the field (ex: 'test@email.com')
+            // The value is always the main document ID.
+            if (new_value !== undefined) {
+              yield* btree.insert(new_value as any, new_doc!.id);
+            }
+          }),
+        { discard: true, concurrency: "inherit" }
+      );
+
+    /**
+     * Finds the IDs of documents that match a given field value.
+     * @param field_name The name of the field to search.
+     * @param value The value to search for.
+     * @returns An effect that returns the IDs of matching documents.
+     */
+    const findIds = (field_name: string, value: unknown) =>
+      Effect.gen(function* () {
+        const btree = btree_services[field_name];
+        if (!btree) {
+          return yield* Effect.fail(
+            new Error(`Index on field ${field_name} does not exist`)
+          );
+        }
+
+        // Delegate to B-tree service to find the document IDs
+        const found_id = yield* btree.find(value as any);
+
+        // Return an array of IDs (or empty array if not found)
+        return found_id ? [found_id] : [];
+      });
+
+    return { update, findIds };
+  });
