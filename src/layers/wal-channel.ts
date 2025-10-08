55d2011870e54b8c018e48762c001a7971a431a5
realfakenerd <lucas210898@hotmail.com>
2025-10-08 10:28:54 -0300

    refactor: remove unused transaction and wal channel services
    The transaction service and WAL channel implementations were unused in the codebase. This commit removes these unused components to simplify the codebase and reduce maintenance overhead.

--- src/layers/wal-channel.ts
+++
@@ -1,6 +0,0 @@
-import { Effect, PubSub } from "effect";
-import type { WALOperation } from "../types/wal.js";
-
-export class WalChannel extends Effect.Service<WalChannel>()("WalChannel", {
-  effect: PubSub.unbounded<WALOperation>()
-}) {}
